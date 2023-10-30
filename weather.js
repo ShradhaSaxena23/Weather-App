5d3ad53eb354609619619999ae398df59aa7be8b
ShradhaSaxena23 <103318017+ShradhaSaxena23@users.noreply.github.com>
2023-10-30 23:37:11 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,43 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '70e288ba13mshd9b694554ec648bp126bc3jsn3f0956b017c3',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather=(city)=>{
+cityName.innerHTML=city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,options)
+.then(response=>response.json())
+.then((response)=>{
+
+	console.log(response)
+
+	cloud_pct.innerHTML=response.cloud_pct
+	cloud_pct2.innerHTML=response.cloud_pct
+	temp.innerHTML=response.temp
+	feels_like.innerHTML=response.feels_like
+	humidity.innerHTML=response.humidity
+	
+	min_temp.innerHTML=response.min_temp
+	max_temp.innerHTML=response.max_temp
+	wind_speed.innerHTML=response.wind_speed
+	wind_speed2.innerHTML=response.wind_speed
+	wind_degrees.innerHTML=response.wind_degrees
+	sunrise.innerHTML=response.sunrise
+	sunset.innerHTML=response.sunset
+	
+	})
+.catch(err=>console.error(err));
+}
+
+submit.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
+  
+getWeather("Lucknow")
+  
+  
+  
